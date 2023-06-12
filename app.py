3ac1aabf239d10d369479e3bc4f65b1b257dc89b
cympfh <cympfh@gmail.com>
2023-06-12 11:48:09 +0900

    update

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ cur = con.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS t(name, x)")
 
 
-limit: int = streamlit.number_input("limit", value=10, min_value=0, max_value=1000, step=1)
+limit: int = streamlit.number_input(
+    "limit", value=10, min_value=0, max_value=1000, step=1
+)
 res = cur.execute("SELECT name, x FROM t LIMIT ?", (limit,))
 con.commit()
 rows: list[tuple[str, int]] = res.fetchall()
@@ -18,10 +20,12 @@ if not rows:
     rows = [("a", 1), ("b", 2), ("c", 3)][:limit]
 
 streamlit.write("## fetched table")
-streamlit.dataframe(rows)
+streamlit.dataframe(rows, use_container_width=True)
 
 streamlit.write("## data editor")
-updated_rows = streamlit.experimental_data_editor(rows)
+updated_rows = streamlit.experimental_data_editor(
+    rows, num_rows="dynamic", use_container_width=True
+)
 streamlit.write(updated_rows)
 
 # update
@@ -30,4 +34,3 @@ if streamlit.button("Update"):
     cur.executemany("INSERT INTO t VALUES(?, ?)", updated_rows)
     con.commit()
     streamlit.balloons()
-
